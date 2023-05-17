c732ba28013b6a6b8b1bdace44820856b039c2d1
avaki11 <azzavaki@gmail.com>
2023-05-17 17:50:56 +1000

    Add put post and delete methods for api response

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,6 @@ app.use(express.json());
 const port = 3000;
 
 //Issues can be hard-coded JSON objects with just 3 attributes: id, title + description.
-
 // Need to Create an empty object to store this issues.
 
 // Create an option to enable Create: accepts a JSON object & prints/logs the object
@@ -39,5 +38,54 @@ app.get('/issues/:id', (response, request) => {
     response.json(getIssue(issues, id));
 });
 
+app.get('/issues/', (response, request) => {
+    const id = request.params.id;
+    const IssuesList = Object.keys(issues).map(id => getIssue(issues, id));
+    response.json(getIssue(issues, IssuesList));
+});
+
+app.post('/issues/', (response, request) => {
+    const {id, title, description} = request.body;
+
+    if(CheckIssue(issues, id)) {
+        response.status(400).send(`Issue with id ${id} already exists`);
+    }
+
+    issues[id] = {
+            title,
+            description
+        };
+        response.status(200).send(`Issue with id ${id} created`);
+});
+
+app.put('/issues/', (response, request) => {
+    const {id, title: newIssueTitle, description: newIssueDescription} = request.body;
+
+    if(!CheckIssue(issues, id)) {
+            response.status(404).send(`Issue with id ${id} does not exist`);
+        }
+        const issue = issues[id];
+        issues[id] = {
+            title: newIssueTitle ? newIssueTitle : issue.title,
+            description:  newIssueDescription ? newIssueDescription : issue.description,
+        };
+        response.status(200).send(`Issue with id ${id} updated`);
+
+});
+
+
+app.delete('/issues/:id', (response, request) => {
+    const id = request.params.id;
+
+    if(!CheckIssue(issues, id)) {
+            response.status(404).send(`Issue with id ${id} does not exist`);
+        }
+
+        delete issues[id];
+        response.status(200).send(`Issue with id ${id} deleted`); 
+
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
